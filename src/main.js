9a0353d97237e77b157986d357f1cc01aba9a0e9
NataliaGurova <nyxnata@ukr.net>
2024-02-11 22:50:06 +0200

    error

--- src/main.js
+++ src/main.js
@@ -3,93 +3,55 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import searchImages from "./js/pixabay-api"
+import showGallery from "./js/render-functions"
+
 
 const textInput = document.querySelector("#form");
 const button = document.querySelector(".button");
 const gallery = document.querySelector(".gallery");
-
-const options = {
-  captions: true,
-  captionType: 'attr',
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionSelector: "img",
-  captionDelay: 250,
-};
+const block = document.querySelector(".block");
 
 
 textInput.addEventListener("submit", handleSubmit);
 
+// Function to show the loader
+const showLoader = () => {
+  const loader = document.createElement('span');
+  loader.classList.add('loader');
+  loader.textContent = "Loading images, please wait...";
+  block.append(loader);
+};
+
+// Function to hide the loader
+const hideLoader = () => {
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    loader.remove();
+  }
+};
 
 function handleSubmit(e) {
   e.preventDefault();
-  
   const userSearch = textInput.elements.query.value.trim();
 
   if (userSearch === "") {
     iziToast.show({
         position: 'topRight',
         color: 'red', // blue, red, green, yellow
-        message: `"Sorry, there are no images matching your search query. Please try again!"`,
+        message: "Sorry, there are no images matching<br>your search query. Please try again!",
       });
   } else {
-    searchName(userSearch);
-    const lightbox = new SimpleLightbox('.gallery a', options);
-    lightbox.on("show.simplelightbox");
-    lightbox.refresh();
-
+    showLoader();
+    searchImages(userSearch);
     }; 
-    console.log(userSearch);
   textInput.reset();
   removeGallery();
-  }
-
-
-function searchName(userSearch) {
-  const apiKey = "42275750-ff2dba3de74b1266fbd0f53be";
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${userSearch}&image_type=photo`
-  return fetch(url)
-    .then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      showGallery(data);
-
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-}
-
-function showGallery(data) {
-  const markup = data.hits
-    .map((item) => {
-      const { largeImageURL, webformatURL, tags, likes, views, comments, downloads } = item;
-      return `
-        <li class="gallery-item">
-          <a class="gallery-link" href="${largeImageURL}">
-          <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
-          </a>
-          <div>
-            <p><b>Likes </b>${likes}</p>
-            <p><b>Views </b>${views}</p>
-            <p><b>Comments </b>${comments}</p>
-            <p><b>Downloads </b>${downloads}</p>
-          </div>
-        </li>
-      `;
-    })
-    .join("");
-  gallery.insertAdjacentHTML("beforeend", markup);
-}
+};
 
 
 function removeGallery() {
   gallery.innerHTML = '';
-}
+};
 
 
